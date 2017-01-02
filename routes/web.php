475a96d125dfea31b51274158457e855aa1640c3
khawGlad <gladyskhaw3095@gmail.com>
2017-01-02 20:57:06 +0800

    commit

--- routes/web.php
+++ routes/web.php
@@ -42,6 +42,11 @@ Route::group(['middleware' => ['web']], function () {
 		'as' => 'account.save'
 		]);
 
+	Route::get('/userimage/{filename}', [
+		'uses' => 'UserController@getUserImage',
+		'as' => 'account.image'	
+		]);
+
 	Route::get('/dashboard', [
 		'uses' => 'PostController@getDashboard',
 		'as' => 'dashboard',
